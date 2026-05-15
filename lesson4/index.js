9f66b115735f09b25ae5b1b2a54ae545dd3ad8c5
tai.pham <tai15122003311@gmail.com>
2026-05-15 20:37:07 +0700

    set up mongo atlas

--- lesson4/index.js
+++ lesson4/index.js
@@ -1,6 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+
+// atlas db
+const url_db =
+  "mongodb+srv://tai15122003311_db_user:HlLVk0btbLIUA0wY@cluster0.mc2s1zi.mongodb.net/fullstack-web?appName=Cluster0";
+
 const app = express();
 
-app.listen(8080, () => {
-  console.log("Server is running!");
-});
+mongoose
+  .connect(url_db)
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(8080, () => {
+      console.log("Server is running!");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
