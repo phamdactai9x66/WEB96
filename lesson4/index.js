66907a285eb17da58149250b83dfcf0aa8517b26
tai.pham <tai15122003311@gmail.com>
2026-05-15 20:39:50 +0700

    local db

--- lesson4/index.js
+++ lesson4/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 
 // atlas db
-const url_db =
-  "mongodb+srv://tai15122003311_db_user:HlLVk0btbLIUA0wY@cluster0.mc2s1zi.mongodb.net/fullstack-web?appName=Cluster0";
+// const url_db =
+//   "mongodb+srv://tai15122003311_db_user:HlLVk0btbLIUA0wY@cluster0.mc2s1zi.mongodb.net/fullstack-web?appName=Cluster0";
+
+//   local db
+const url_db = "mongodb://localhost:27017/fullstack-web";
 
 const app = express();
 
