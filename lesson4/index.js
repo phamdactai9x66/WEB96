9b75e446a62674c60145526e9a6df221ee2f13e3
tai.pham <tai15122003311@gmail.com>
2026-05-25 19:54:56 +0700

    lesson 7

--- lesson4/index.js
+++ lesson4/index.js
@@ -15,12 +15,19 @@ import customerRouter from "./routers/customers.js";
 
 import auth from "./middlewares/auth.js";
 
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 //   local db
 const url_db = "mongodb://localhost:27017/fullstack-web";
 
 const app = express();
 
 app.use(express.json());
+
+console.log(`process.env.DATABASE_URL: ${process.env.DATABASE_URL}`);
 
 function myLogger(req, res, next) {
   console.log(`Received request for: ${req.url}`);
@@ -37,12 +44,59 @@ app.use("/customers", customerRouter);
 // get all users == Admin
 app.use("/users", auth.auhthorizationAdmin, userRouter);
 
+const saltRounds = 10;
+
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  // tạo chuỗi ngẫu nhiên
+  const salt = bcrypt.genSaltSync(saltRounds);
+  // thực hiện mã hoá với chuỗi salt
+  const hash = bcrypt.hashSync(password, salt);
+
+  const newUser = await UsersModel.create({ email, password: hash, salt });
+
+  res.status(201).json({
+    message: "Register with hash password!",
+    userInfo: newUser,
+    success: true,
+  });
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // tìm thông tin user | tài khoản với email được gửi lên
+    const currentUser = await UsersModel.findOne({ email });
+
+    if (!currentUser) throw new Error("Sai tài khoản hoặc mật khẩu");
+
+    const hashingPasswordLogin = bcrypt.hashSync(password, currentUser.salt);
+
+    // compare password
+    if (hashingPasswordLogin !== currentUser.password)
+      throw new Error("Sai tài khoản hoặc mật khẩu");
+
+    res.status(201).send({
+      message: "Login successfully!",
+      email,
+      userInfo: currentUser,
+      // v.v user info
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+      data: null,
+      success: false,
+    });
+  }
+});
+
 mongoose
   .connect(url_db)
   .then(() => {
     console.log("Connected to MongoDB");
 
-    app.listen(8080, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server is running!");
     });
   })
