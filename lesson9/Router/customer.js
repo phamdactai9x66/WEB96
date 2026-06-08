4ff5c03576306dca1d3eaf73a581dce39f111c7c
tai.pham <tai15122003311@gmail.com>
2026-06-08 20:10:45 +0700

    add pagination, filter, sort

--- lesson9/Router/customer.js
+++ lesson9/Router/customer.js
@@ -1,10 +1,63 @@
 import express from "express";
 import customerController from "../controller/customer.js";
 
+import CustomerModel from "../Model/Customers.js";
+
 const router = express.Router();
 
 router.post("/", customerController.registerCustomer);
 
 router.post("/login", customerController.loginCustomer);
+
+// get all customers
+router.get("/", async (req, res) => {
+  try {
+    const { pageSize = 10, pageNumber = 1 } = req.query;
+    // code to get all customers from database
+    const totalItems = await CustomerModel.countDocuments();
+
+    const totalPages = Math.ceil(totalItems / pageSize);
+    // Tính toán vị trí bắt đầu của trang hiện tại, trừ 1 vì mảng bắt đầu từ vị trí 0
+    const skip = (pageNumber - 1) * pageSize;
+
+    let condition = {};
+    if (req.query.search) {
+      condition.name = { $regex: req.query.search, $options: "i" };
+    }
+
+    // Truy vấn dữ liệu sử dụng Mongoose
+    const result = await CustomerModel.find(condition)
+      .skip(skip)
+      .limit(pageSize);
+
+    const data = {
+      totalItems,
+      totalPages,
+      currentPage: +pageNumber,
+      items: result,
+    };
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: "Error getting customers" });
+  }
+});
+
+// get all customers
+router.get("/customers_v2", async (req, res) => {
+  try {
+    const { search = "" } = req.query;
+    //     const result = await YourModel.find({
+    //     userName: 'MindX'
+    // });
+
+    const result = await CustomerModel.find({
+      name: { $regex: search, $options: "i" },
+    });
+
+    res.json({ items: result });
+  } catch (error) {
+    res.status(500).json({ message: "Error getting customers" });
+  }
+});
 
 export default router;
