8dd7f73c6aaa1e2b2c699dc468282be2820fdd7d
tai.pham <tai15122003311@gmail.com>
2026-06-11 00:03:13 +0700

    update project

--- lesson1/index.js
+++
@@ -1,5 +0,0 @@
-const utils = require("./utils.js");
-
-const format = utils.formatPhoneNumber("2345678900");
-console.log(format);
-// (234) 567-8900
--- lesson4/routers/users.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userController from "../controller/users.js";
-
-const router = express.Router();
-
-// get all users
-router.get("/", userController.getAllUsers);
-
-export default router;
